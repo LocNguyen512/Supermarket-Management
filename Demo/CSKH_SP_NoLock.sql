20cb4d34372e40cdf8be71b033c8886653f72073
Vy <lnyvy22@clc.fitus.edu.vn>
2024-12-27 22:11:10 +0700

    UPDATE

--- Demo/CSKH_SP_NoLock.sql
+++ Demo/CSKH_SP_NoLock.sql
@@ -1,56 +1,153 @@
-﻿USE QLSIEUTHI
+﻿USE SupermarketDB
 
--- STORED PROCEDURE THÊM MÃ GIẢM GIÁ
-CREATE PROCEDURE SPTHEMPMH
-    @SODIENTHOAIKH CHAR(10),
-    @NGAYHIEULUC DATETIME,
-    @NGAYHETHAN DATETIME
+--TẶNG PHIẾU MUA HÀNG CHO KHÁCH HÀNG
+CREATE PROC SP_TANGPHIEUMUAHANG
 AS
 BEGIN
-    DECLARE @IDPHIEUMUAHANG VARCHAR(10);
-    DECLARE @GIATRI INT;
-    DECLARE @ISPHIEUMUAHANGEXIST BIT = 1;
-    DECLARE @MUCKHTT NVARCHAR(10);
+    SET NOCOUNT ON;
 
-    -- KIỂM TRA NGÀY HIỆU LỰC VÀ NGÀY HẾT HẠN
-    IF @NGAYHIEULUC >= @NGAYHETHAN
+    -- Khai báo biến
+    DECLARE @SoDienThoai CHAR(10);
+    DECLARE @NgaySinh DATE;
+    DECLARE @MucKHTT NVARCHAR(10);
+    DECLARE @NgayBatDau DATE = GETDATE();
+    DECLARE @NgayKetThuc DATE = DATEADD(DAY, 30, @NgayBatDau); -- Phiếu có hạn 30 ngày
+    DECLARE @MaPhieu NVARCHAR(50);
+
+    -- Cursor để duyệt qua khách hàng
+    DECLARE KhachHangSNCursor CURSOR FOR
+    SELECT SODIENTHOAI, NGAYSINH, MUCKHTT
+    FROM KHACHHANG;
+
+    OPEN KhachHangSNCursor;
+
+    FETCH NEXT FROM KhachHangSNCursor INTO @SoDienThoai, @NgaySinh, @MucKHTT;
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
-        RAISERROR('NGÀY HIỆU LỰC KHÔNG ĐƯỢC LỚN HƠN HOẶC BẰNG NGÀY HẾT HẠN', 16, 1);
-        RETURN;
-    END
-
-    -- TẠO MÃ PHIEUMUAHANG NGẪU NHIÊN
-    WHILE @ISPHIEUMUAHANGEXIST = 1
-    BEGIN
-        SET @IDPHIEUMUAHANG = RIGHT(CONCAT('0000000000', CAST(ABS(CHECKSUM(NEWID())) % 10000000000 AS VARCHAR(10))), 10);
-
-        -- KIỂM TRA MÃ PHIEUMUAHANG ĐÃ TỒN TẠI CHƯA
-        IF NOT EXISTS (SELECT 1 FROM PHIEUMUAHANG WHERE MAPHIEUMUAHANG = @IDPHIEUMUAHANG)
+        -- Kiểm tra ngày sinh có nằm trong khoảng xét
+        IF CONVERT(VARCHAR(5), @NgaySinh, 110) BETWEEN 
+           CONVERT(VARCHAR(5), @NgayBatDau, 110) AND 
+           CONVERT(VARCHAR(5), DATEADD(MONTH, 1, @NgayBatDau), 110)
         BEGIN
-            SET @ISPHIEUMUAHANGEXIST = 0;
-        END
-    END
+            -- Tạo mã phiếu duy nhất
+            WHILE 1 = 1
+            BEGIN
+                SET @MaPhieu = CAST(NEWID() AS NVARCHAR(50));
 
-    -- LẤY MỨC KHTT TỪ BẢNG KHACHHANG
-    SET @MUCKHTT = (SELECT MUCKHTT FROM KHACHHANG WHERE SODIENTHOAI = @SODIENTHOAIKH);
+                -- Kiểm tra nếu mã đã tồn tại
+                IF NOT EXISTS (SELECT 1 FROM PHIEUMUAHANG WHERE MAPHIEUMUAHANG = @MaPhieu)
+                    BREAK; -- Thoát vòng lặp nếu mã là duy nhất
+            END;
 
-    -- THIẾT LẬP GIÁ TRỊ DỰA TRÊN MỨC KHTT
-    IF @MUCKHTT = N'KIM CƯƠNG'
-        SET @GIATRI = 1200000;
-    ELSE IF @MUCKHTT = N'BẠCH KIM'
-        SET @GIATRI = 700000;
-    ELSE IF @MUCKHTT = N'VÀNG'
-        SET @GIATRI = 500000;
-    ELSE IF @MUCKHTT = N'BẠC'
-        SET @GIATRI = 200000;
-    ELSE IF @MUCKHTT = N'ĐỒNG'
-        SET @GIATRI = 100000;
-    ELSE
-    BEGIN
-        RETURN;
-    END
+            -- Thêm phiếu mua hàng cho khách hàng
+            INSERT INTO PHIEUMUAHANG (MAPHIEUMUAHANG, SODIENTHOAI, NGAYHIEULUC, NGAYHETHAN, GIATRI, TRANGTHAI)
+            VALUES (
+                @MaPhieu,
+                @SoDienThoai,                   -- Số điện thoại khách hàng
+                @NgayBatDau,                   -- Ngày hiệu lực
+                @NgayKetThuc,                  -- Ngày hết hạn
+                CASE @MucKHTT                   -- Giá trị phiếu dựa vào mức KHTT
+                    WHEN N'Kim cương' THEN 1200000
+                    WHEN N'Bạch kim' THEN 700000
+                    WHEN N'Vàng' THEN 500000
+                    WHEN N'Bạc' THEN 200000
+                    WHEN N'Đồng' THEN 100000
+                    ELSE 50000
+                END,
+                1                               -- Trạng thái kích hoạt
+            );
+        END;
 
-    -- THÊM PHIEUMUAHANG VÀO BẢNG PHIEUMUAHANG
-    INSERT INTO PHIEUMUAHANG (MAPHIEUMUAHANG, SODIENTHOAIKH, NGAYHIEULUC, NGAYHETHAN, GIATRI, KHADUNG)
-    VALUES (@IDPHIEUMUAHANG, @SODIENTHOAIKH, @NGAYHIEULUC, @NGAYHETHAN, @GIATRI, 1);
+        -- Lấy khách hàng tiếp theo
+        FETCH NEXT FROM KhachHangSNCursor INTO @SoDienThoai, @NgaySinh, @MucKHTT;
+    END;
+
+    -- Đóng và giải phóng cursor
+    CLOSE KhachHangSNCursor;
+    DEALLOCATE KhachHangSNCursor;
+
+    SET NOCOUNT OFF;
 END;
+GO
+
+
+
+--Cập nhật hạng khách hàng thân thiết
+CREATE PROC SP_CAPNHATKHTT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Biến lưu ngày hiện tại
+    DECLARE @NgayHienTai DATE = GETDATE();
+    DECLARE @SoDienThoai CHAR(10);
+    DECLARE @NgayXet DATE;
+    DECLARE @TongChiTieu INT;
+	DECLARE @HangMoi NVARCHAR(10);
+
+    -- Cursor để duyệt qua khách hàng
+    DECLARE KhachHangCursor CURSOR FOR
+    SELECT SODIENTHOAI, NGAYXETKHTT
+    FROM KHACHHANG;
+
+    OPEN KhachHangCursor;
+
+    FETCH NEXT FROM KhachHangCursor INTO @SoDienThoai, @NgayXet;
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Nếu NGAYXET là NULL, lấy NGAYDANGKI làm ngày bắt đầu
+        IF @NgayXet IS NULL
+        BEGIN
+            SELECT @NgayXet = NGAYDANGKI FROM KHACHHANG WHERE SODIENTHOAI = @SoDienThoai;
+        END
+
+        -- Kiểm tra điều kiện thời gian (1 năm từ ngày xét cuối cùng)
+        IF @NgayHienTai >= DATEADD(YEAR, 1, @NgayXet)
+        BEGIN
+            -- Tính tổng chi tiêu trong khoảng thời gian
+            SELECT @TongChiTieu = SUM(THANHTIEN)
+            FROM LSMUAHANG
+            WHERE SODIENTHOAI = @SoDienThoai
+              AND NGAYTHANHTOAN BETWEEN @NgayXet AND DATEADD(YEAR, 1, @NgayXet);
+
+            -- Kiểm tra điều kiện nâng hạng
+            IF @TongChiTieu >= 50000000 -- Điều kiện nâng hạng
+            BEGIN
+                SET @HangMoi = N'Kim cương';
+            END
+			ELSE IF @TongChiTieu < 50000000 and @TongChiTieu >= 30000000
+			BEGIN
+                SET @HangMoi = N'Bạch kim';
+            END
+			ELSE IF @TongChiTieu < 30000000 and @TongChiTieu >= 15000000
+			BEGIN
+                SET @HangMoi = N'Vàng';
+            END
+			ELSE IF @TongChiTieu < 15000000 and @TongChiTieu >= 5000000
+			BEGIN
+                SET @HangMoi = N'Bạc';
+            END
+			ELSE IF @TongChiTieu < 5000000 and @TongChiTieu > 0
+			BEGIN
+                SET @HangMoi = N'Đồng';
+            END
+			ELSE
+			BEGIN
+                SET @HangMoi = N'Thân thiết';
+            END
+        END
+		UPDATE KHACHHANG
+			SET MUCKHTT = @HangMoi, NgayXetKHTT = DATEADD(YEAR, 1, @NgayXet)
+			WHERE SODIENTHOAI = @SoDienThoai
+
+        -- Fetch next customer
+        FETCH NEXT FROM KhachHangCursor INTO @SoDienThoai, @NgayXet;
+    END
+
+    CLOSE KhachHangCursor;
+    DEALLOCATE KhachHangCursor;
+END;
+
+
